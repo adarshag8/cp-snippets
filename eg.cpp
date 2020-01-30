23282ee421cbd2424110f2139f6c933d0d26dc5b
Adarsh <adarshagr8@gmail.com>
2020-01-30 15:04:08 +0530

    added new snippets

---
+++ eg.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define M 1000000007
+#define sz(c) c.size()
+#define fr first
+#define sc second
+#define pb push_back
+#define sz(c) c.size()
+#define ppb  pop_back
+#define all(a)      (a).begin(),(a).end()
+#define what_is(x)  cerr << #x << " is " << x << endl;
+
+
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int TESTS=1;
+	cin>>TESTS;
+	while(TESTS--)
+	{
+
+	}
+}
